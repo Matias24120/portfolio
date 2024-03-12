86888c45c0dc71580d42828e5bd9c498b5fcf737
Matías <137196771+Matias24120@users.noreply.github.com>
2024-03-12 19:23:45 -0300

    Update ErrorModal.jsx

--- src/components/ErrorModal.jsx
+++ src/components/ErrorModal.jsx
@@ -20,14 +20,14 @@ const ErrorModal = ({ onClose }) => {
 
   return (
     <div className="fixed z-10 inset-0 overflow-y-auto bg-black bg-opacity-80 flex justify-center items-center">
-      <div className="bg-white border-2 border-red-500 rounded-lg overflow-hidden p-8 max-w-lg w-full mx-4">
+      <div className="bg-white border-2 border-red-500 rounded-lg overflow-hidden p-8 pt-4 pr-6 max-w-lg w-full mx-4">
         <div className="flex justify-end">
           <button onClick={onClose} className="text-gray-400 hover:text-red-500 focus:outline-none">
             <FaTimes className="h-6 w-6 sm:h-8 sm:w-8 text-white bg-red-500 hover:bg-red-400 rounded-full p-1"/>
           </button>
         </div>
-        <h2 className="text-lg sm:text-2xl font-bold mb-4 mt-4">{isEnglish ? 'Form submitted unsuccessfully' : 'Formulario enviado sin éxito'}</h2>
-        <p className="text-md sm:text-lg text-gray-600">{isEnglish ? 'Oops, something went wrong. Please try again.' : 'Ups! Algo salió mal. Por favor inténtalo de nuevo.'}</p>
+        <h2 className="text-lg sm:text-2xl font-bold pr-6 mb-4 mt-4">{isEnglish ? 'Form submitted unsuccessfully' : 'Formulario enviado sin éxito'}</h2>
+        <p className="text-md sm:text-lg pr-8 text-gray-600">{isEnglish ? 'Oops, something went wrong. Please try again.' : 'Ups! Algo salió mal. Por favor inténtalo de nuevo.'}</p>
       </div>
     </div>
   );
