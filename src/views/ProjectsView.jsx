8d42d19665dcf2fc4aa2f472a0003eec5e4d6c81
Matías <137196771+Matias24120@users.noreply.github.com>
2024-03-12 23:04:40 -0300

    Update ProjectsView.jsx

--- src/views/ProjectsView.jsx
+++ src/views/ProjectsView.jsx
@@ -30,20 +30,16 @@ const ProjectsView = () => {
   }, []);
 
   const toggleAccordion = (index) => {
-    if (selectedProject === index) {
-      setSelectedProject(null);
-    } else {
-      setSelectedProject(index);
-      setCurrentImageIndex(0); 
-    }
+    setSelectedProject((prevIndex) => (prevIndex === index ? null : index));
+    setCurrentImageIndex(0);
   };
 
   const nextSlide = () => {
-    setCurrentImageIndex(prevIndex => (prevIndex === projects[selectedProject].images.length - 1 ? 0 : prevIndex + 1));
+    setCurrentImageIndex((prevIndex) => (prevIndex === projects[selectedProject].images.length - 1 ? 0 : prevIndex + 1));
   };
   
   const prevSlide = () => {
-    setCurrentImageIndex(prevIndex => (prevIndex === 0 ? projects[selectedProject].images.length - 1 : prevIndex - 1));
+    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? projects[selectedProject].images.length - 1 : prevIndex - 1));
   };
 
   const getIcons = (icons) => {
@@ -88,44 +84,42 @@ const ProjectsView = () => {
             <h3 className="accordion-title text-xl sm:text-3xl text-white">{project.title}</h3>
             <div className="accordion-arrow text-blue-500 text-2xl sm:text-3xl">{selectedProject === index ? <FaArrowUp /> : <FaArrowDown />}</div>
           </div>
-          {selectedProject === index && (
-            <div className="accordion-content flex flex-col lg:flex-row">
-              <div className='lg:w-1/2 relative'>
-                <div className="slider-container relative" style={{ width: '100%', overflow: 'hidden' }}>
-                  <div className="slider-inner" style={{ display: 'flex', transition: 'transform 0.5s ease', transform: `translateX(-${currentImageIndex * 100}%)` }}>
-                    {project.images.map((image, index) => (
-                      <img key={index} src={image} alt={project.title} className="p-4 w-full" />
-                    ))}
-                  </div>
+          <div className={`accordion-content flex flex-col lg:flex-row ${selectedProject === index ? 'active' : ''}`} style={{ maxHeight: selectedProject === index ? '1000px' : '0', overflow: 'hidden', transition: 'max-height 0.5s ease' }}>
+            <div className='lg:w-1/2 relative'>
+              <div className="slider-container relative" style={{ width: '100%', overflow: 'hidden' }}>
+                <div className="slider-inner" style={{ display: 'flex', transition: 'transform 0.5s ease', transform: `translateX(-${currentImageIndex * 100}%)` }}>
+                  {project.images.map((image, index) => (
+                    <img key={index} src={image} alt={project.title} className="p-4 w-full" />
+                  ))}
                 </div>
-                <div className="slider-nav">
-                  <button className="prev-slide" onClick={prevSlide}>&#10094;</button>
-                  <button className="next-slide" onClick={nextSlide}>&#10095;</button>
-                </div>
-                <div className="hidden lg:block absolute top-6 bottom-6 right-0 bg-white w-px"></div>
-                <div className="lg:hidden absolute bottom-0 right-5 left-5 bg-white h-px"></div>
               </div>
-              <div className='lg:w-1/2 p-5 lg:pl-3.5 pt-2.5 lg:pt-5 flex flex-col justify-between text-white'>
-                <p className="mb-4 text-md sm:text-xl">{isEnglish ? project.description.en : project.description.es}</p>
-                <div className="flex justify-between">
-                  <div className="flex items-end text-3xl sm:text-4xl">
-                    {getIcons(project.icons)}
-                    {project.tailwindIcon && <img src={project.tailwindIcon} alt="Tailwind Icon" style={{ objectFit: 'contain', marginLeft: '4px' }} className='tailwind-img' />}
-                  </div>
-                  <div className="flex items-end text-3xl sm:text-4xl">
-                    {index >= projects.length - 3 ? (
-                      <FaBan />
-                    ) : (
-                      <>
-                        <a href={project.github} className='git-hover' target="_blank" rel="noreferrer"><FaGithub /></a>
-                        <a href={project.website} className='web-hover ml-2 sm:ml-4' target="_blank" rel="noreferrer"><FaExternalLinkAlt /></a>
-                      </>
-                    )}
-                  </div>
+              <div className="slider-nav">
+                <button className="prev-slide" onClick={prevSlide}>&#10094;</button>
+                <button className="next-slide" onClick={nextSlide}>&#10095;</button>
+              </div>
+              <div className="hidden lg:block absolute top-6 bottom-6 right-0 bg-white w-px"></div>
+              <div className="lg:hidden absolute bottom-0 right-5 left-5 bg-white h-px"></div>
+            </div>
+            <div className='lg:w-1/2 p-5 lg:pl-3.5 pt-2.5 lg:pt-5 flex flex-col justify-between text-white'>
+              <p className="mb-4 text-md sm:text-xl">{isEnglish ? project.description.en : project.description.es}</p>
+              <div className="flex justify-between">
+                <div className="flex items-end text-3xl sm:text-4xl">
+                  {getIcons(project.icons)}
+                  {project.tailwindIcon && <img src={project.tailwindIcon} alt="Tailwind Icon" style={{ objectFit: 'contain', marginLeft: '4px' }} className='tailwind-img' />}
+                </div>
+                <div className="flex items-end text-3xl sm:text-4xl">
+                  {index >= projects.length - 3 ? (
+                    <FaBan />
+                  ) : (
+                    <>
+                      <a href={project.github} className='git-hover' target="_blank" rel="noreferrer"><FaGithub /></a>
+                      <a href={project.website} className='web-hover ml-2 sm:ml-4' target="_blank" rel="noreferrer"><FaExternalLinkAlt /></a>
+                    </>
+                  )}
                 </div>
               </div>
             </div>
-          )}
+          </div>
           {index === 3 && (
             <div className="accordion mt-8">
               <p className="text-lg sm:text-2xl text-gray-400 flex items-center">
